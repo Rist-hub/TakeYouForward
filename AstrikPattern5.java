34481ebb23ede921cc74ac31944fff6929d27e26
Rishabh Sharma <rishabh981215@gmail.com>
2026-03-07 23:45:48 +0530

    AstrikPattern5.java

---
+++ AstrikPattern5.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class AstrikPattern5{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=n; i>0; i--){
+            for(int j=0; j<n-i; j++){
+                System.out.print(" ");
+            }
+            for(int k=0; k<i+i-1; k++){
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+    }
+}
