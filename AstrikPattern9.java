8b66cada4f498074c829b6556bdd92be661de43d
Rishabh Sharma <rishabh981215@gmail.com>
2026-03-17 16:07:24 +0530

    AstrikPattern9.java

---
+++ AstrikPattern9.java
@@ -0,0 +1,49 @@
+
+import java.util.Scanner;
+
+public class AstrikPattern9{
+    public static void main(String[] args){
+        /*
+
+        **********
+        ****  ****
+        ***    ***
+        **      **
+        *        *
+        *        *
+        **      **
+        ***    ***
+        ****  ****
+        **********
+
+        */
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        String astrik = "";
+        for(int row=0; row<n-1; row++){
+            for(int i=0; i<row+1; i++){
+                System.out.print("*");
+            }
+            for(int i=2*(n-row)-2; i>0; i--){
+                System.out.print(" ");
+            }
+            for(int i=0; i<row+1; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int row=0; row<n; row++){
+            for(int i=n-row; i>0; i--){
+                System.out.print("*");
+            }
+            for(int i=0; i<2*row; i++){
+                System.out.print(" ");
+            }
+            for(int i=n-row; i>0; i--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
