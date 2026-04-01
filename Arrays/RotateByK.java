536f6773ddcacb86c188757daf3f8f9aea8ab04c
Rishabh Sharma <rishabh981215@gmail.com>
2026-04-01 19:18:12 +0530

    Rotate elements of the array left or right by K indexes, removed sysouts

--- Arrays/RotateByK.java
+++ Arrays/RotateByK.java
@@ -4,26 +4,20 @@ public class RotateByK{
         if(direction=="left"){
             while(l<=k){
                 int temp = arr[0];
-                System.out.println("------------------------------ "+l);
                 for(int i=1; i<arr.length; i++){
                     arr[i-1]=arr[i];
-                    System.out.println("arr[i-1] : "+arr[i-1]);
                 }
                 arr[arr.length-1] = temp;
-                System.out.println("arr[arr.length-1] : "+arr[arr.length-1]);
                 l++;
             }
         }
         else if(direction=="right"){
             while(l<=k){
                 int temp = arr[arr.length-1];
-                System.out.println("------------------------------ "+l);
                 for(int i=arr.length-2; i>=0; i--){
                     arr[i+1]=arr[i];
-                    System.out.println("arr[i+1] : "+arr[i+1]);
                 }
                 arr[0] = temp;
-                System.out.println("arr[0] : "+arr[0]);
                 l++;
             }
         }else{
