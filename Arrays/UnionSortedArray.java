050d8c7e439c9aa8a3dba4344abc82e13cc6bdfc
Rishabh Sharma <rishabh981215@gmail.com>
2026-04-05 23:52:05 +0530

    Union of two sorted arrays updated

--- Arrays/UnionSortedArray.java
+++ Arrays/UnionSortedArray.java
@@ -2,27 +2,24 @@ public class UnionSortedArray{
     public static void union(int[] arr1, int[] arr2){
         int[] union = new int[arr1.length + arr2.length];
         int index = 0;
-        for(int i=0; i<arr1.length; i++){
-            for(int j=i+1; j<arr2.length; j++){
-                if(arr1[i]<arr2[j]){
-                    union[index] = arr1[i];
-                    index++;
-                }else{
-                    union[index] = arr2[j];
-                    index++;
-                }
+        for(int j=0, i =0; j<arr1.length && i<arr2.length; j++, i++){
+            if(arr1[j]<=arr2[i]){
+                union[index] = arr1[j];    
+                System.out.println("arr1[j] : "+arr1[j]);
+            }else{
+                union[index] = arr2[i];
+                System.out.println("arr2[i] : "+arr2[i]);
             }
+            index++;
         }
-
-
         for(int u=0; u<union.length; u++){
             System.out.print(union[u]+", ");
         }
     }
 
     public static void main(String[] args){
-        int[] arr1 = {1,2,3,4,6};
-        int[] arr2 = {5,7,8,9};
+        int[] arr1 = {1,2,4,6};
+        int[] arr2 = {3,5,7,8,9};
 
         union(arr1, arr2);
     }
