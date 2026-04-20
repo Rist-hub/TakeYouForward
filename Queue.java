3dd84730af9277d6c4c5a39cfe678f8ca06d63fb
Rishabh Sharma <58072640+Rist-hub@users.noreply.github.com>
2026-04-20 23:53:50 +0530

    Queue.java

---
+++ Queue.java
@@ -0,0 +1,11 @@
+public class Queue{
+    public static void main(String[] args){
+        int[] arr= new int[5];
+
+        arr[2] = 12;
+        arr[4] = 7;
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
